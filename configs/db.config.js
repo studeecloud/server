dfac9a3ca3574843b90041c10ac7bdceee694e5c
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-24 08:54:16 -0400

    now correctly pulling db connection info out of environment vars

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,8 +1,19 @@
 const { Pool } = require('pg');
-console.log(process.env);
-// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, PORT } = process.env;
+const DATABASE_URL = processs.env.DATABASE_URL;
+const SPLIT_DATABASE_URL = DATABASE_URL.split(/(postgres:\/\/)|:|@|\//)
+  .splice(2)
+  .filter((x) => x !== undefined);
 
-const pool = new Pool(process.env.DATABASE_URL);
+const [DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE] =
+  SPLIT_DATABASE_URL;
+
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  port: DB_PORT,
+  database: DB_DATABASE,
+});
 
 pool
   .connect()
