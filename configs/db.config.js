457a467d32ac1b4e9734561ea041237d02da8feb
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-24 08:28:14 -0400

    testing db

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,20 +1,22 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT} = process.env;
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, PORT } = process.env;
 
-const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_DATABASE
-});
+// const pool = new Pool({
+//   user: DB_USER,
+//   host: DB_HOST,
+//   password: DB_PASSWORD,
+//   port: PORT,
+//   database: DB_DATABASE,
+// });
 
-pool.connect()
-    .then(() => {
-      console.log('Database connection established.');
-    }).catch(err => {
-      throw new Error(err);
-    })
+pool
+  .connect()
+  .then(() => {
+    console.log('Database connection established.');
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
 
 module.exports = pool;
