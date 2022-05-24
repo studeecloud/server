87e7180402c887efcd1a091615fd2dc7ea700d99
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-24 08:34:07 -0400

    more db testing

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,14 +1,8 @@
 const { Pool } = require('pg');
+console.log(process.env);
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, PORT } = process.env;
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, PORT } = process.env;
-
-const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  port: PORT,
-  database: DB_DATABASE,
-});
+const pool = new Pool({ DATABASE_URL });
 
 pool
   .connect()
