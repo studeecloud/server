79a73073418470f7c5e2587713b32460c00bebef
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-24 08:56:43 -0400

    fix typo

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-const DATABASE_URL = processs.env.DATABASE_URL;
+const { DATABASE_URL } = process.env;
 const SPLIT_DATABASE_URL = DATABASE_URL.split(/(postgres:\/\/)|:|@|\//)
   .splice(2)
   .filter((x) => x !== undefined);
